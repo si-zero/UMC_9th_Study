67405169ade0ea8b58fae18a89373f4e66c3c6f6
si-zero <snm306@naver.com>
2025-10-14 14:19:06 +0900

    feat: 회원가입 요청 추가

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.post("/api/user/register", requestUserRegister);
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
