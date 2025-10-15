42f60eae269d061e6946e3d6bc09e5f760cf4a47
si-zero <snm306@naver.com>
2025-10-15 16:20:09 +0900

    feat: 유저 회원가입 서비스 관련 파일 추가

---
+++ src/services/user.service.js
@@ -0,0 +1,28 @@
+import { responseFromUser } from "~/dtos/user.dto.js";
+import { addUser, getUser, getUserPreferencesByUserId, setPreference, } from "~/repositories/user.repository.js";
+
+export const userSignUp = async (data) => {
+  const joinUserId = await addUser({
+    name: data.name, // "유시영"
+    nickname: data.nickname || data.name, // "벡스" 또는 "유시영"
+    email: data.email || "", // "snm306@naver.com" 또는 ""
+    gender: data.gender, // "남"
+    phoneNumber: data.phoneNumber, // "010-1234-1234"
+    status: data.status || "미인증", // "미인증"
+    date: data.date, // "2003-06-11"
+    address: data.address || "", // "경기도 고리울로39번길 4"
+    favorite_id: data.favorite_id || [] // [1, 2] 또는 []
+  });
+
+  if (joinUserId === null) {
+    throw new Error("이미 존재하는 이메일입니다.");
+  }
+
+  for (const preference of data.preferences) {
+    await setPreference(joinUserId, preference);
+  }
+
+  const user = await getUser(joinUserId);
+  const preferences = await getUserPreferencesByUserId(joinUserId);
+  return responseFromUser({ user, preferences });
+};
